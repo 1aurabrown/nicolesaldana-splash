03fbc7f4a0654a9297ebddb19d0003f3dbd779cd
Laura Brown <laurabrown1113@gmail.com>
2017-07-18 21:19:28 +0200

    look good at all screen sizes

--- assets/coffee/index.coffee
+++ assets/coffee/index.coffee
@@ -1,41 +1,77 @@
-$(document).ready ->
-  ajaxMailChimpForm $("#subscribe-form"), $("#subscribe-success"), $("#subscribe-error")
-  # Turn the given MailChimp form into an ajax version of it.
-  # If resultElement is given, the subscribe result is set as html to
-  # that element.
+class EmailForm
 
+  constructor: (@$form, @$email, @$placeholder, @$success, @$errorEl) ->
+    @$email.on 'focus', @hidePlaceholder
+    @$email.on 'blur', @showPlaceholder
+    @$form.submit @onSubmit
+    @clearNotices()
 
-ajaxMailChimpForm = ($form, $successEl, $errorEl) ->
+  clearNotices: =>
+    @$errorEl.css 'visibility', 'hidden'
+    @$success.css 'visibility', 'hidden'
 
-  $form.submit (e) ->
+  showError: ->
+    @$errorEl.css 'visibility', 'visible'
+
+  showSuccessAndDisable: ->
+    @$success.css 'visibility', 'visible'
+    @$email.attr('disabled','disabled').css('visibility', 'hidden')
+    @$form.find("input[type='submit']").attr('disabled','disabled')
+
+  showPlaceholder: =>
+    return if @$email[0].value.length
+    @$errorEl.css 'visibility', 'hidden'
+    @$placeholder.css 'visibility', 'visible'
+
+  hidePlaceholder: =>
+    @$placeholder.css 'visibility', 'hidden'
+
+  onSubmit: (e) =>
+    # Hijack submit event and submit via ajax
     e.preventDefault()
-    $errorEl.html ""
-    return if not $form.find("input[type='email']")[0].value.length
-    unless $form[0].checkValidity()
-      return $errorEl.html "Please check the email you entered!"
+    @clearNotices()
+    return if not @$email[0].value.length
+    unless @$form[0].checkValidity()
+      return @showError()
+    @submit()
 
-    submitSubscribeForm $form, $successEl, $errorEl
+  # Submit the form with an ajax/jsonp request.
+  # Based on http://stackoverflow.com/a/15120409/215821
+  submit:  ->
+    @$email.prop('disabled', true)
+    @$form.find("input[type='submit']").prop('disabled', true)
+    $.ajax
+      type: "GET",
+      url: @$form.attr("action").replace('/post', '/post-json'),
+      data: @$form.serialize(),
+      cache: false,
+      dataType: "jsonp",
+      jsonp: "c", # trigger MailChimp to return a JSONP response
+      contentType: "application/json; charset=utf-8",
+      error: (error) -> # According to jquery docs, this is never called for cross-domain JSONP requests
+      success: (data) =>
+        if (data.result == "success") || (data.msg && data.msg.indexOf("already subscribed") >= 0)
+          @showSuccessAndDisable()
+        else
+          @$email.prop('disabled', false)
+          @$form.find("input[type='submit']").prop('disabled', false)
+          @showError()
 
-# Submit the form with an ajax/jsonp request.
-# Based on http://stackoverflow.com/a/15120409/215821
-submitSubscribeForm = ($form, $successEl, $errorEl) ->
-  $successEl.hide
-  $errorEl.html ''
-  $.ajax
-    type: "GET",
-    url: $form.attr("action").replace('/post', '/post-json'),
-    data: $form.serialize(),
-    cache: false,
-    dataType: "jsonp",
-    jsonp: "c", # trigger MailChimp to return a JSONP response
-    contentType: "application/json; charset=utf-8",
-    error: (error) -> # According to jquery docs, this is never called for cross-domain JSONP requests
-    success: (data) ->
-      if (data.result == "success") || (data.msg && data.msg.indexOf("already subscribed") >= 0)
-        $successEl.show()
-        $form.find("input[type='email']").hide()
-        $successEl.html "thank you, we will be in touch soon!"
-        $successEl.show()
-      else
-        $errorEl.html "Please check the email you entered!"
+classForViewport = ->
+  ratio = $(window).width()/$(window).height()
+  if ratio < .9
+    $('body').addClass 'is-skinny'
+  else
+    $('body').removeClass 'is-skinny'
 
+$(document).ready ->
+  classForViewport()
+  $(window).resize _.debounce =>
+    classForViewport()
+  window.form = new EmailForm(
+    $("#subscribe-form"),
+    $("#subscribe-form input[type='email']"),
+    $("#subscribe-form #fake-placeholder"),
+    $("#subscribe-form #subscribe-success"),
+    $("#subscribe-error")
+  )
--- public/js/index.js
+++ public/js/index.js
@@ -1,44 +1,108 @@
-var ajaxMailChimpForm, submitSubscribeForm;
+var EmailForm, classForViewport,
+  bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-$(document).ready(function() {
-  return ajaxMailChimpForm($("#subscribe-form"), $("#subscribe-success"), $("#subscribe-error"));
-});
+EmailForm = (function() {
+  function EmailForm($form, $email, $placeholder, $success, $errorEl) {
+    this.$form = $form;
+    this.$email = $email;
+    this.$placeholder = $placeholder;
+    this.$success = $success;
+    this.$errorEl = $errorEl;
+    this.onSubmit = bind(this.onSubmit, this);
+    this.hidePlaceholder = bind(this.hidePlaceholder, this);
+    this.showPlaceholder = bind(this.showPlaceholder, this);
+    this.clearNotices = bind(this.clearNotices, this);
+    this.$email.on('focus', this.hidePlaceholder);
+    this.$email.on('blur', this.showPlaceholder);
+    this.$form.submit(this.onSubmit);
+    this.clearNotices();
+  }
 
-ajaxMailChimpForm = function($form, $successEl, $errorEl) {
-  return $form.submit(function(e) {
-    e.preventDefault();
-    $errorEl.html("");
-    if (!$form.find("input[type='email']")[0].value.length) {
+  EmailForm.prototype.clearNotices = function() {
+    this.$errorEl.css('visibility', 'hidden');
+    return this.$success.css('visibility', 'hidden');
+  };
+
+  EmailForm.prototype.showError = function() {
+    return this.$errorEl.css('visibility', 'visible');
+  };
+
+  EmailForm.prototype.showSuccessAndDisable = function() {
+    this.$success.css('visibility', 'visible');
+    this.$email.attr('disabled', 'disabled').css('visibility', 'hidden');
+    return this.$form.find("input[type='submit']").attr('disabled', 'disabled');
+  };
+
+  EmailForm.prototype.showPlaceholder = function() {
+    if (this.$email[0].value.length) {
       return;
     }
-    if (!$form[0].checkValidity()) {
-      return $errorEl.html("Please check the email you entered!");
+    this.$errorEl.css('visibility', 'hidden');
+    return this.$placeholder.css('visibility', 'visible');
+  };
+
+  EmailForm.prototype.hidePlaceholder = function() {
+    return this.$placeholder.css('visibility', 'hidden');
+  };
+
+  EmailForm.prototype.onSubmit = function(e) {
+    e.preventDefault();
+    this.clearNotices();
+    if (!this.$email[0].value.length) {
+      return;
     }
-    return submitSubscribeForm($form, $successEl, $errorEl);
-  });
+    if (!this.$form[0].checkValidity()) {
+      return this.showError();
+    }
+    return this.submit();
+  };
+
+  EmailForm.prototype.submit = function() {
+    this.$email.prop('disabled', true);
+    this.$form.find("input[type='submit']").prop('disabled', true);
+    return $.ajax({
+      type: "GET",
+      url: this.$form.attr("action").replace('/post', '/post-json'),
+      data: this.$form.serialize(),
+      cache: false,
+      dataType: "jsonp",
+      jsonp: "c",
+      contentType: "application/json; charset=utf-8",
+      error: function(error) {},
+      success: (function(_this) {
+        return function(data) {
+          if ((data.result === "success") || (data.msg && data.msg.indexOf("already subscribed") >= 0)) {
+            return _this.showSuccessAndDisable();
+          } else {
+            _this.$email.prop('disabled', false);
+            _this.$form.find("input[type='submit']").prop('disabled', false);
+            return _this.showError();
+          }
+        };
+      })(this)
+    });
+  };
+
+  return EmailForm;
+
+})();
+
+classForViewport = function() {
+  var ratio;
+  ratio = $(window).width() / $(window).height();
+  if (ratio < .9) {
+    return $('body').addClass('is-skinny');
+  } else {
+    return $('body').removeClass('is-skinny');
+  }
 };
 
-submitSubscribeForm = function($form, $successEl, $errorEl) {
-  $successEl.hide;
-  $errorEl.html('');
-  return $.ajax({
-    type: "GET",
-    url: $form.attr("action").replace('/post', '/post-json'),
-    data: $form.serialize(),
-    cache: false,
-    dataType: "jsonp",
-    jsonp: "c",
-    contentType: "application/json; charset=utf-8",
-    error: function(error) {},
-    success: function(data) {
-      if ((data.result === "success") || (data.msg && data.msg.indexOf("already subscribed") >= 0)) {
-        $successEl.show();
-        $form.find("input[type='email']").hide();
-        $successEl.html("Thank you!");
-        return $successEl.show();
-      } else {
-        return $errorEl.html("Please check the email you entered!");
-      }
-    }
-  });
-};
+$(document).ready(function() {
+  classForViewport();
+  $(window).resize(_.debounce((function(_this) {
+    return function() {
+      return classForViewport();
+    };
+  })(this)));
+  return window.form = new EmailForm($("#subscribe-form"), $("#subscribe-form input[type='email']"), $("#subscribe-form #fake-placeholder"), $("#subscribe-form #subscribe-success"), $("#subscribe-error"));
+});
